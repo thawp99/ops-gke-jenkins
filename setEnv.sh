ab50a3832c6f7acbcfa895ddf9ea2b45d2ebcfbc
Brett Curtis <brett@lzy.sh>
2017-10-23 14:28:08 -0400

    Adding Codecov, seperated variables from test file and changed jenkins container name
    Test script setup was put into an environment file foe easier support of different environments.
    
    Jenkins container name was set to master and that was confusing when looking at monitoring tools. Changed to jenkins-master
    in verious places.

--- setEnv.sh
+++ setEnv.sh
@@ -1,3 +1,11 @@
 # environment
 export yamlDir="yaml"
 export functions="ops-common/functions"
+
+# test script setup
+testName=jenkins-test
+project=ops-iac-sb
+zone=us-east1-b
+clusterName=${testName}-cluster
+domainName=${testName}.ois.lzy.sh
+cleanup=true
--- test.sh
+++ test.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-# test script setup
-testName=jenkins-test
-project=ops-iac-sb
-zone=us-east1-b
-clusterName=${testName}-cluster
-domainName=${testName}.ois.lzy.sh
-cleanup=true
-
 # set environment
 source setEnv.sh
 
