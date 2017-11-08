4c6e4c4f74486a9c7cc7dc2e62df45745a3984c4
Brett Curtis <brett@lzy.sh>
2017-11-08 08:52:57 -0500

    Testing environment changes

--- setEnv.sh
+++ setEnv.sh
@@ -1,6 +1,13 @@
-# environment
-export yamlDir="yaml"
+# environment setup
 export functions="ops-common/functions"
+gcloudDebug=false
+
+# gcloud verbosity
+if [ "${gcloudDebug}" = true ]; then
+        export gcloud="gcloud -q --verbosity=debug"
+else
+        export gcloud="gcloud -q --verbosity=error --no-user-output-enabled"
+fi
 
 # test script setup
 testName=jenkins-test
